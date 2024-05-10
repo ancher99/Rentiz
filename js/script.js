fac8db061684be71735358cff85ed6b93bd85cf4
anton <antonchervyakov2003@bk.ru>
2024-05-10 16:03:25 +0300

    add js oce

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ if(burger && menu){
         body.classList.toggle('_lock')
     })
 }
+
+
+//---Filter dropdown
+
+const filter = document.querySelector('.filter')
+
+if (filter){
+    const items = filter.querySelectorAll('.block-filter')
+  
+    items.forEach(item =>{
+        item.addEventListener('click', event =>{
+            item.querySelector('.block-filter__dropdown').classList.toggle('_active');
+            item.querySelector('.block-filter__icon').classList.toggle('_active');
+
+            if(event.target.classList.contains('block-filter__item')){
+                item.querySelector('.block-filter__value').textContent = event.target.textContent;
+            }
+        })
+    })
+}
